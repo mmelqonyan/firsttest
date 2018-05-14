fe18a2e8f84977ea52ad8f39585fbdce0239fca0
mher <mmelqonyan1990@gmail.com>
2018-05-15 03:25:29 +0400

    orer

---
+++ oreri.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <cstdlib>
+struct Date{
+    int year;
+    int month;
+    int day;
+};
+
+int main() {
+    int days[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int yr;
+    int mt;
+    int dy;
+  Date  first;
+  std::cout << "Taretiv  ";
+  std::cin >> yr;
+  if( 1980 < yr &&  2100 > yr){
+  first.year=yr;
+  } else {
+      std::cout << "@ntreq urish taretiv";
+      exit(0);
+  }
+  std::cout << "Amis   ";
+  std::cin >> mt;
+  if( 0 < mt && 13 > mt){
+     first.month = mt; 
+  } else {
+      std::cout << "tarin uni 12 amis";
+      exit(0);
+  }
+   std::cout << "Amsativ   ";
+  std::cin >> dy;
+  if( dy <= days[mt] && dy > 0){
+     first.day = dy; 
+  } else {
+      std::cout << "Oreri qanak@ sxal e";
+      exit(0);
+  }
+  std::cout << first.year << first.month << first.day;
+  
+  
+  
+  Date sec;//2nd date
+   std::cout << "Taretiv2  ";
+  std::cin >> yr;
+  if( 1980 < yr &&  2100 > yr && yr <=  first.year){
+  sec.year=yr;
+  } else {
+      std::cout << "@ntreq urish taretiv";
+      exit(0);
+  }
+  std::cout << "Amis2   ";
+  std::cin >> mt;
+  if( 0 < mt && 13 > mt ){
+     sec.month = mt; 
+  } else {
+      std::cout << "tarin uni 12 amis";
+      exit(0);
+  }
+   std::cout << "Amsativ   ";
+  std::cin >> dy;
+  int dcont;
+  int dcont1;
+  if( dy <= days[mt] && dy > 0){
+     sec.day = dy; 
+  } else {
+      std::cout << "Oreri qanak@ sxal e";
+      exit(0);
+  }
+    
+    if(sec.month > first.month && sec.day > first.day){
+        for(int i = first.month - 1 ; i <= sec.month; ++i) {
+            dcont += days[i];
+        }
+        std::cout << "Oreri qanak@= " << (sec.year - first.year) * 365 + dcont + (sec.day - first.day);
+    }else if(sec.year > first.year && sec.month < first.month && sec.day > first.day ){
+         for(int i = first.month - 1 ; i < 12; ++i){
+             dcont += days[i];
+         }
+         for
+        std::
+    }
+    
+    
+    
+    
+       std::cout << sec.year << sec.month << sec.day;
+ return 0;  
+}
